4aa78a4ba82367609aa4288ae80f4bdc0b1aa416
vitor.padilha@uniriotec.br <vitor.padilha@uniriotec.br@c91a3a02-7a77-6f2d-5def-1e6a23e6b549>
2012-11-24 13:54:57 +0000

    Acerto.

--- trunk/ESE/src/br/uniriotec/ese/GeraInformacoesEstatisticas.java
+++ trunk/ESE/src/br/uniriotec/ese/GeraInformacoesEstatisticas.java
@@ -40,48 +40,46 @@ public class GeraInformacoesEstatisticas {
 
 		fileWriterPacotes.append("# Tabela com informacoes do EVM para as aplicações para sobol e pseudo\r\n");
 		fileWriterPacotes.append("tabelaEVM<-read.table(\""+nomeTabelaEVM+".txt\",h=T,sep=\";\")\r\n");
-		
-		fileWriterPacotes.append("#################################\r\n");
-		fileWriterPacotes.append("# Calcula min, max e desvio padrão para MQ\r\n");
-		fileWriterPacotes.append("#################################\r\n");
-		fileWriterPacotes.append("minimosMQPseudo <- NULL\r\n");
-		fileWriterPacotes.append("maximosMQPseudo <- NULL\r\n");
-		fileWriterPacotes.append("desvioPadraoMQPseudo <- NULL\r\n");
-		fileWriterPacotes.append("mediaMQPseudo <- NULL\r\n");
-		fileWriterPacotes.append("valoresMQSobol <- NULL\r\n");
-		fileWriterPacotes.append("wilcoxMQValues <- NULL\r\n");
-		fileWriterPacotes.append("effectSizeMQValues <- NULL\r\n");
-		fileWriterPacotes.append("i<-1\r\n");
+		String[] informacoes = new String[]{"MQ","EVM"};
+		for (String informacao : informacoes) {
+			fileWriterPacotes.append("#################################\r\n");
+			fileWriterPacotes.append("# Calcula min, max e desvio padrão para "+informacao+"\r\n");
+			fileWriterPacotes.append("#################################\r\n");
+			fileWriterPacotes.append("minimos"+informacao+"Pseudo <- NULL\r\n");
+			fileWriterPacotes.append("maximos"+informacao+"Pseudo <- NULL\r\n");
+			fileWriterPacotes.append("desvioPadrao"+informacao+"Pseudo <- NULL\r\n");
+			fileWriterPacotes.append("media"+informacao+"Pseudo <- NULL\r\n");
+			fileWriterPacotes.append("valores"+informacao+"Sobol <- NULL\r\n");
+			fileWriterPacotes.append("wilcox"+informacao+"Values <- NULL\r\n");
+			fileWriterPacotes.append("effectSize"+informacao+"Values <- NULL\r\n");
+			fileWriterPacotes.append("i<-1\r\n");
 
-		fileWriterPacotes.append("tabelaMQPSEUDO<-subset(tabelaMQ,tabelaMQ$"+ParserSimpl.CABECALHO_DISTRIBUICAO+"=='"+DistribuicaoDosDados.PSEUD.name()+"')\r\n");
-		fileWriterPacotes.append("tabelaMQSOBOL<-subset(tabelaMQ,tabelaMQ$"+ParserSimpl.CABECALHO_DISTRIBUICAO+"=='"+DistribuicaoDosDados.SOBOL.name()+"')\r\n");
+			fileWriterPacotes.append("tabela"+informacao+"PSEUDO<-subset(tabela"+informacao+",tabela"+informacao+"$"+ParserSimpl.CABECALHO_DISTRIBUICAO+"=='"+DistribuicaoDosDados.PSEUD.name()+"')\r\n");
+			fileWriterPacotes.append("tabela"+informacao+"SOBOL<-subset(tabela"+informacao+",tabela"+informacao+"$"+ParserSimpl.CABECALHO_DISTRIBUICAO+"=='"+DistribuicaoDosDados.SOBOL.name()+"')\r\n");
+			
+			fileWriterPacotes.append("aplicacoes"+informacao+"<-names(table(tabela"+informacao+"PSEUDO$"+ParserSimpl.CABECALHO_APLICACAO+"))\r\n");
+			
+			fileWriterPacotes.append("for ( proj in aplicacoes"+informacao+" ) { \r\n");
+			fileWriterPacotes.append("	tabela"+informacao+"PSEUDOProj<-subset(tabela"+informacao+"PSEUDO,tabela"+informacao+"PSEUDO$"+ParserSimpl.CABECALHO_APLICACAO+"==proj)\r\n");	
+			fileWriterPacotes.append("	tabela"+informacao+"SOBOLProj<-subset(tabela"+informacao+"SOBOL,tabela"+informacao+"SOBOL$"+ParserSimpl.CABECALHO_APLICACAO+"==proj)\r\n");	
+			fileWriterPacotes.append("	tamanho<-nrow(tabela"+informacao+"SOBOLProj)\r\n");	
+			fileWriterPacotes.append("	tamanhoPSEUD<-nrow(tabela"+informacao+"PSEUDOProj)\r\n");	
+			fileWriterPacotes.append("	minimos"+informacao+"Pseudo[proj]<-min(tabela"+informacao+"PSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+")\r\n");
+			fileWriterPacotes.append("	maximos"+informacao+"Pseudo[proj]<-max(tabela"+informacao+"PSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+")\r\n");
+			fileWriterPacotes.append("	media"+informacao+"Pseudo[proj]<-mean(tabela"+informacao+"PSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+")\r\n");
+			fileWriterPacotes.append("	desvioPadrao"+informacao+"Pseudo[proj]<-sd(tabela"+informacao+"PSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+")\r\n");
+			fileWriterPacotes.append("	wilcox"+informacao+"Values[proj]<-wilcox.test(tabela"+informacao+"PSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+",tabela"+informacao+"SOBOLProj$"+ParserSimpl.CABECALHO_VALOR+")$p.value\r\n");
+			fileWriterPacotes.append("	valores"+informacao+"Sobol[proj]<-tabela"+informacao+"SOBOLProj$"+ParserSimpl.CABECALHO_VALOR+"[1]\r\n");
+			
+			fileWriterPacotes.append("	rx <- sum(rank(c(tabela"+informacao+"PSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+", tabela"+informacao+"SOBOLProj$"+ParserSimpl.CABECALHO_VALOR+"))[seq_along(tabela"+informacao+"PSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+")])\r\n");
+			
+			fileWriterPacotes.append("	effectSize"+informacao+"Values[proj]<-((rx / tamanhoPSEUD - (tamanhoPSEUD + 1) / 2) / tamanho) \r\n");
+			fileWriterPacotes.append("	i<-i+1\r\n");
+			fileWriterPacotes.append("}\r\n");
+			fileWriterPacotes.append("resultados"+informacao+"<-data.frame(valores"+informacao+"Sobol,minimos"+informacao+"Pseudo,media"+informacao+"Pseudo,desvioPadrao"+informacao+"Pseudo,maximos"+informacao+"Pseudo,wilcox"+informacao+"Values,effectSize"+informacao+"Values)\r\n");
+			fileWriterPacotes.append("resultados"+informacao+"\r\n");
+		}
 		
-		fileWriterPacotes.append("aplicacoes<-names(table(tabelaMQPSEUDO$"+ParserSimpl.CABECALHO_APLICACAO+"))\r\n");
-		
-		fileWriterPacotes.append("for ( proj in aplicacoes ) { \r\n");
-		fileWriterPacotes.append("	tabelaMQPSEUDOProj<-subset(tabelaMQPSEUDO,tabelaMQPSEUDO$"+ParserSimpl.CABECALHO_APLICACAO+"==proj)\r\n");	
-		fileWriterPacotes.append("	tabelaMQSOBOLProj<-subset(tabelaMQSOBOL,tabelaMQSOBOL$"+ParserSimpl.CABECALHO_APLICACAO+"==proj)\r\n");	
-		fileWriterPacotes.append("	tamanho<-nrow(tabelaMQSOBOLProj)\r\n");	
-		fileWriterPacotes.append("	minimosMQPseudo[proj]<-min(tabelaMQPSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+")\r\n");
-		fileWriterPacotes.append("	maximosMQPseudo[proj]<-max(tabelaMQPSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+")\r\n");
-		fileWriterPacotes.append("	mediaMQPseudo[proj]<-mean(tabelaMQPSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+")\r\n");
-		fileWriterPacotes.append("	desvioPadraoMQPseudo[proj]<-sd(tabelaMQPSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+")\r\n");
-		fileWriterPacotes.append("	wilcoxMQValues[proj]<-wilcox.test(tabelaMQPSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+",tabelaMQSOBOLProj$"+ParserSimpl.CABECALHO_VALOR+")$p.value\r\n");
-		fileWriterPacotes.append("	valoresMQSobol[proj]<-tabelaMQSOBOLProj$"+ParserSimpl.CABECALHO_VALOR+"[1]\r\n");
-		
-		fileWriterPacotes.append("	rx <- sum(rank(c(tabelaMQPSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+", tabelaMQSOBOLProj$"+ParserSimpl.CABECALHO_VALOR+"))[seq_along(tabelaMQPSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+")])\r\n");
-
-		
-		
-		fileWriterPacotes.append("	effectSizeMQValues[proj]<-(rx / tamanho - (tamanho + 1) / 2)\r\n");
-		
-		
-		fileWriterPacotes.append("	wilcox.test(tabelaMQSOBOLProj$"+ParserSimpl.CABECALHO_VALOR+",tabelaMQPSEUDOProj$"+ParserSimpl.CABECALHO_VALOR+")\r\n");
-		
-		fileWriterPacotes.append("	i<-i+1\r\n");
-		fileWriterPacotes.append("}\r\n");
-		fileWriterPacotes.append("resultadosMQ<-data.frame(valoresMQSobol,minimosMQPseudo,mediaMQPseudo,desvioPadraoMQPseudo,maximosMQPseudo,wilcoxMQValues,effectSizeMQValues)\r\n");
-		fileWriterPacotes.append("resultadosMQ\r\n");
 		
 		
 		
